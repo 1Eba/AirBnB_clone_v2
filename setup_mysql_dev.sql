8e816408fb7aa209c36399a5df993ca07191a25f
root <1Eba>
2022-03-10 13:13:36 -0800

    MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,15 +1,9 @@
--- Creating database call hbnb_test_db
+-- Creating hbnb_dev_db database
+-- Privileges for new user (hbnb_dev)
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- Creating user hbnbn_test and set password
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
-IDENTIFIED BY 'hbnb_dev_pwd';
--- Giving all permissions to the database hbnbn_test_db
-GRANT ALL PRIVILEGES ON hbnb_dev_db.*
-TO 'hbnb_dev'@'localhost'
-IDENTIFIED BY 'hbnb_dev_pwd';
--- Giving permission SELECT to the database performance_schema
-GRANT SELECT ON performance_schema.*
-TO 'hbnb_dev'@'localhost'
-IDENTIFIED BY 'hbnb_dev_pwd';
--- Refresh all priviliges
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
+USE hbnb_dev_db;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
